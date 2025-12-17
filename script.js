692d1a722338b329cc811ce60383ea919ceeb66a
Rema770 <iiinininiii3@gmail.com>
2025-12-17 16:19:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
 const icon = themeToggle.querySelector('i');
 
-// التحقق من وجود تفضيل سابق للمستخدم في المتصفح
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme === 'dark') {
     body.setAttribute('data-theme', 'dark');
@@ -10,7 +9,6 @@ if (currentTheme === 'dark') {
 }
 
 themeToggle.addEventListener('click', () => {
-    // تبديل السمات
     if (body.getAttribute('data-theme') === 'dark') {
         body.removeAttribute('data-theme');
         icon.classList.replace('fa-sun', 'fa-moon');
@@ -20,4 +18,5 @@ themeToggle.addEventListener('click', () => {
         icon.classList.replace('fa-moon', 'fa-sun');
         localStorage.setItem('theme', 'dark');
     }
+
 });
